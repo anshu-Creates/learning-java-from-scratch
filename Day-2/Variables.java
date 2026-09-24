961d589f649925544395983c7a085bb10efd1e29
Anshu Kumar <anshu.builds@gmail.com>
2026-09-24 20:06:45 +0530

    Day-2 : Ouput, Variables, Data Types & User Input

---
+++ Day-2/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 20;
+        String name = "Anshu";
+        double percent = 99.99;
+        char grade = 'A';
+        boolean isPass = true;
+
+        System.out.println(name + " whose age is " + age + " scored grade " + grade + " with a percentage of " + percent
+                + " and this news is " + isPass+".");
+    }
+}
---
+++ Day-2/output.java
@@ -0,0 +1,9 @@
+public class output {
+    public static void main(String[] args){
+        System.out.print("Hello World");
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.print("Hello\n World");
+    }
+    
+}
